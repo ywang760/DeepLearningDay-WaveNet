2252d529912b7338f30a4d06c7d374ec0cf5c708
ywang760 <yutong_wang5@brown.edu>
2022-12-05 11:09:36 -0500

    cant test final_project.ipynb, please review

---
+++ generate.py
@@ -0,0 +1,11 @@
+import numpy as np
+import tensorflow as tf
+
+
+class Generate:
+    def __init__(self):
+        pass
+
+    # TODO: not sure how the predicted_output looks like
+    def generate(self, predicted_output):
+        pass
