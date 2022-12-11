f2874fd87679b1fd63d453f9259f30c0869aa057
ywang760 <yutong_wang5+cs1300@brown.edu>
2022-12-11 17:27:40 -0500

    added readme

--- README.md
+++ README.md
@@ -1,12 +1,44 @@
-# DeepLearningFinal-WaveNet
+# Music Generation with WaveNet
 
-## Requirements
-Install the following packages:
-pip install ffmpeg
-pip install pydub
-pip install librosa
+This is an adaptation of [WaveNet: A Generative Model for Raw Audio](https://arxiv.org/pdf/1609.03499.pdf) for music generation using MIDI and WAV audio files. This is a final project for CSCI 1470 Deep Learning at Brown University, created by Yutong Wang, Zhijun Liu, Rosanna Zhao, and Shuyang Song.
+A detailed description of the project can be found [here](https://devpost.com/software/music-generation-with-wavenet).
 
-https://www.analyticsvidhya.com/blog/2020/01/how-to-perform-automatic-music-generation/
+This model depends on TensorFlow 2, which can be installed with ```pip install tensorflow```.
 
-TODO list:
-1. re-write crawler code
+# Usage
+
+## Audio files in MIDI format
+This section is adapted from [this repository](https://github.com/soumya997/Music-Generation-Using-Deep-Learning).
+We modified it to become an easy-to-use object-oriented API. 
+
+To run: ```python3 wavenet.py piano/ 128 100```
+```piano/``` is the directory containing the MIDI files. 
+```128``` is the batch size for training the model. 
+```100``` is the number of epochs to train the model.
+
+You might need to install the following packages:
+```pip install music21```
+
+Example MIDI files can be found in the ```piano/``` directory.
+```final_project.ipynb``` is a Jupyter notebook that demonstrates how to use the API.
+```example output.mid``` is an example of the output MIDI file.
+
+
+## Audio files in WAV format
+To run: use the ```final_project.ipynb``` notebook.
+
+Possible parameters to adjust:
+time_steps, number of samples per input
+sampling_rate, number of samples per second
+quantization_channels, number of possible values for each sample
+batch_size, number of samples per batch
+epochs, number of epochs to train the model
+
+You might need to install the following packages:
+```pip install ffmpeg```
+```pip install pydub```
+```pip install librosa```
+```pip install soundfile```
+```pip install matplotlib```
+
+Example mp3 files can be found in the ```guzheng/``` directory. The code can convert mp3 files to WAV files.
---
+++ music generation with WAV/wavenet.py
@@ -0,0 +1,55 @@
+import numpy as np
+import tensorflow as tf 
+import tensorflow.keras.layers as layers
+import soundfile as sf
+
+def mu_law_decode(signal, quantization_channels):
+    # Calculate inverse mu-law companding and dequantization
+    mu = quantization_channels - 1
+    # Map signal from [0, mu-1] to [-1, +1]
+    signal = 2 * (signal.astype(np.float32) / mu) - 1
+    signal = np.sign(signal) * (1.0 / mu) * ((1.0 + mu)**abs(signal) - 1.0)
+    return signal
+
+class Wavenet(tf.keras.Model):
+    def __init__(self, timesteps = 32, quantization_channels=256, **kwargs):
+        super().__init__(**kwargs)
+        self.timesteps = timesteps
+        self.quantization_channels = quantization_channels
+        self.model = tf.keras.Sequential([
+            layers.Embedding(self.quantization_channels, 100, input_length=32, trainable=True),
+            layers.Conv1D(64, 3, padding='causal', activation='relu'),
+            layers.Dropout(0.2),
+            layers.MaxPool1D(2),
+            layers.Conv1D(128, 3, activation='relu', dilation_rate=2, padding='causal'),
+            layers.Dropout(0.2),
+            layers.MaxPool1D(2),
+            layers.Conv1D(256, 3, activation='relu', dilation_rate=4, padding='causal'),
+            layers.Dropout(0.2),
+            layers.MaxPool1D(2),
+            layers.GlobalMaxPool1D(),
+            layers.Dense(256, activation='relu'),
+            layers.Dense(self.quantization_channels, activation='softmax'),
+        ])
+
+    def call(self, inputs):
+        return self.model(inputs)
+
+    def generate(self, generate_time, sampling_rate):
+        mean = self.quantization_channels / 2
+        std = mean * 0.909
+        no_samples = generate_time * sampling_rate
+        inputs = tf.random.normal((no_samples, self.timesteps), mean=mean, stddev=std, dtype=tf.float32)
+
+        # forward pass:
+        predicted_output = self.model.predict(inputs)
+        print(f"Model prediction has shape {predicted_output.shape}")
+
+        # generate predictions
+        labels = np.argmax(predicted_output, axis=-1)
+        print(f"Labels has shape {labels.shape}")
+
+        # decode the predictions
+        self.out = mu_law_decode(labels, self.quantization_channels)
+        sf.write("generated.wav", self.out, sampling_rate)
+        print("Finished generating audio")
